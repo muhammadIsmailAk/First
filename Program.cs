447e6184a4b4edd459924beeebf86609a01db857
muhammadIsmailAk <70567934+muhammadIsmailAk@users.noreply.github.com>
2020-11-24 12:36:42 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;int A = 20;int C;C = a + A;
+            Console.WriteLine("sum of two number " + C);
+            Console.ReadKey();
+
+        }
+    }
+}
